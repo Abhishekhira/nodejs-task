b7a3d9e57f9cc94e7506ad8f5420a2b1bc67c37f
abhishek <abhishek.raina@grazitti.com>
2022-12-19 22:37:24 +0530

    new commit `

--- notes.js
+++ notes.js
@@ -7,7 +7,7 @@ function getNotes() {
 function addNotes(title,body) {
     const notes=loadNotes();
 
-   console.log("hii")
+   console.log("hiihiii")
 
     const duplicateNotes=notes.filter(function(note){
      return note.title===title;
